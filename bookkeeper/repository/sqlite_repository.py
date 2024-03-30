c3138a75bf78b627a790e96f8ec0141266386c22
Stanislav <sidelnikov.si.001@gmail.com>
2024-03-30 13:59:24 +0300

    Implement add method

--- bookkeeper/repository/sqlite_repository.py
+++ bookkeeper/repository/sqlite_repository.py
@@ -3,7 +3,10 @@
 """
 
 import sqlite3
-from inspect import get_annotations
+from inspect import getmembers
+from inspect import isroutine
+from typing import Any
+
 from bookkeeper.repository.abstract_repository import AbstractRepository, T
 
 
@@ -14,18 +17,27 @@ class SqliteRepository(AbstractRepository[T]):
 
     def __init__(self, db_file: str, cls: type) -> None:
         self._db_file = db_file
+        self._cls = cls
         self._table_name = cls.__name__.lower()
-        self._fields = get_annotations(cls, eval_str=True)
-        self._fields.pop("pk")
+        attrs = getmembers(cls, lambda a: not(isroutine(a)))
+        self._fields = [a[0] for a in attrs if not(a[0].startswith('__') and a[0].endswith('__'))]
+        self._fields.remove("pk")
 
-    def add(self, obj: T) -> int:
-        names = ", ".join(self._fields.keys())
+
+    def add(self, obj: T) -> int | None:
+        if not isinstance(obj, self._cls):
+            raise ValueError(f"Passing object of type {obj.__class__} instead of {self._cls}")
+
+        names = ", ".join(self._fields)
         p = ", ".join("?" * len(self._fields))
         values = [getattr(obj, x) for x in self._fields]
 
+        if len(values) == 0:
+            return None
+
         with sqlite3.connect(self._db_file) as con:
             cur = con.cursor()
-            cur.execute("PRAGME foreign_keys = ON")
+            cur.execute("PRAGMA foreign_keys = ON")
             cur.execute(
                 f"INSERT INTO {self._table_name} ({names} VALUES ({p}))", values
             )
@@ -33,3 +45,23 @@ class SqliteRepository(AbstractRepository[T]):
         con.close()
 
         return obj.pk
+
+    def get(self, pk: int) -> T | None:
+        with sqlite3.connect(self._db_file) as con:
+            cur = con.cursor()
+            cur.execute(
+                f"SELECT * FROM {self._table_name}" f"WHERE row_number() = {pk}"
+            )
+            data = cur.fetchall()
+        con.close()
+        obj = None
+        return obj
+
+    def get_all(self, where: dict[str, Any] | None = None) -> list[T]:
+        pass
+
+    def update(self, obj: T) -> None:
+        pass
+
+    def delete(self, pk: int) -> None:
+        pass
--- tests/test_repository/test_common_repository.py
+++ tests/test_repository/test_common_repository.py
@@ -3,19 +3,21 @@ from bookkeeper.repository.memory_repository import MemoryRepository
 from bookkeeper.repository.sqlite_repository import SqliteRepository
 
 import pytest
+from pathlib import Path
+
+_repo_path = Path(__file__).parent.parent.parent
+
+class Custom():
+    pk: int = 0
+
+def db_name() -> str:
+    return _repo_path / "tests" / "test_db.db"
 
 
-def custom_class():
-    class Custom():
-        pk = 0
-
-    return Custom
-
-
-@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), custom_class),
-                                               (SqliteRepository(), custom_class)])
+@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), Custom), (SqliteRepository(db_name(), Custom), Custom)])
 def test_crud(repo, custom_class):
     obj = custom_class()
+    import code; code.interact(local={**locals(), **globals()})
     pk = repo.add(obj)
     assert obj.pk == pk
     assert repo.get(pk) == obj
@@ -26,43 +28,37 @@ def test_crud(repo, custom_class):
     repo.delete(pk)
     assert repo.get(pk) is None
 
-@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), custom_class),
-                                               (SqliteRepository(), custom_class)])
+@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), Custom)])
 def test_cannot_add_with_pk(repo, custom_class):
     obj = custom_class()
     obj.pk = 1
     with pytest.raises(ValueError):
         repo.add(obj)
 
-@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), custom_class),
-                                               (SqliteRepository(), custom_class)])
+@pytest.mark.parametrize("repo", [(MemoryRepository())])
 def test_cannot_add_without_pk(repo):
     with pytest.raises(ValueError):
         repo.add(0)
 
-@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), custom_class),
-                                               (SqliteRepository(), custom_class)])
+@pytest.mark.parametrize("repo", [(MemoryRepository())])
 def test_cannot_delete_unexistent(repo):
     with pytest.raises(KeyError):
         repo.delete(1)
 
-@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), custom_class),
-                                               (SqliteRepository(), custom_class)])
+@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), Custom)])
 def test_cannot_update_without_pk(repo, custom_class):
     obj = custom_class()
     with pytest.raises(ValueError):
         repo.update(obj)
 
-@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), custom_class),
-                                               (SqliteRepository(), custom_class)])
+@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), Custom)])
 def test_get_all(repo, custom_class):
     objects = [custom_class() for i in range(5)]
     for o in objects:
         repo.add(o)
     assert repo.get_all() == objects
 
-@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), custom_class),
-                                               (SqliteRepository(), custom_class)])
+@pytest.mark.parametrize("repo,custom_class", [(MemoryRepository(), Custom)])
 def test_get_all_with_condition(repo, custom_class):
     objects = []
     for i in range(5):
