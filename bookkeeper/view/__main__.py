7be0de76a492b57858eb7fce81716b985fcb5479
Stanislav <sidelnikov.si.001@gmail.com>
2024-04-06 00:20:22 +0300

    Add basic table widget

--- bookkeeper/view/__main__.py
+++ bookkeeper/view/__main__.py
@@ -1,13 +1,21 @@
-
 import sys
 from PySide6 import QtWidgets
 
 from .view import Window
+from .view import ExpensesTable
+
 
 def _main() -> None:
     app = QtWidgets.QApplication(sys.argv)
     window = Window("The bookkeeper app")
+
+    expences_table = ExpensesTable(
+        20, ["Дата", "Сумма", "Категория", "Комментарий"]
+    )
+    window.setCentralWidget(expences_table)
+
     window.show()
     sys.exit(app.exec())
+
 
 _main()
--- bookkeeper/view/view.py
+++ bookkeeper/view/view.py
@@ -1,12 +1,26 @@
-
 from PySide6 import QtWidgets
 
-class Window():
-    def __init__(self, title: str, size_x: int = 800, size_y: int = 600) -> None:
-        self._window  = QtWidgets.QWidget()
-        self._window.setWindowTitle(title)
-        self._window.resize(size_x, size_y)
 
-    def show(self) -> None:
-        self._window.show()
+class Window(QtWidgets.QMainWindow):
+    def __init__(
+        self, title: str, size_x: int = 800, size_y: int = 600
+    ) -> None:
+        super().__init__()
+        self.setWindowTitle(title)
+        self.resize(size_x, size_y)
 
+
+class ExpensesTable(QtWidgets.QTableWidget):
+    def __init__(self, nrows: int, headers: list[str]) -> None:
+        super().__init__()
+        ncols = len(headers)
+        self.setColumnCount(ncols)
+        self.setRowCount(nrows)
+        self.setHorizontalHeaderLabels(headers)
+        header = self.horizontalHeader()
+        for col_idx in range(ncols):
+            header.setSectionResizeMode(
+                col_idx, QtWidgets.QHeaderView.ResizeToContents
+            )
+        self.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.verticalHeader().hide()
