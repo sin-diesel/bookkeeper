81779c25d3190405152ac4be95fe0ce521c0d5bd
Stanislav <sidelnikov.si.001@gmail.com>
2024-04-05 23:42:19 +0300

    Add basic window with title

--- bookkeeper/view/__main__.py
+++ bookkeeper/view/__main__.py
@@ -1,10 +1,13 @@
 
 import sys
-
 from PySide6 import QtWidgets
+
+from .view import Window
 
 def _main() -> None:
     app = QtWidgets.QApplication(sys.argv)
+    window = Window("The bookkeeper app")
+    window.show()
     sys.exit(app.exec())
 
 _main()
--- bookkeeper/view/view.py
+++ bookkeeper/view/view.py
@@ -1,6 +1,12 @@
 
+from PySide6 import QtWidgets
 
 class Window():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, title: str, size_x: int = 800, size_y: int = 600) -> None:
+        self._window  = QtWidgets.QWidget()
+        self._window.setWindowTitle(title)
+        self._window.resize(size_x, size_y)
+
+    def show(self) -> None:
+        self._window.show()
 
